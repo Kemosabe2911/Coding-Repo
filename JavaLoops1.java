e600fafcf6572e1b65ea0d24d11a6cf63401248f
Kemosabe2911 <stevinprince29@gmail.com>
2020-06-05 15:41:01 +0530

    Add java loops 1

---
+++ JavaLoops1.java
@@ -0,0 +1,48 @@
+/*
+Sample Input
+
+2
+
+Sample Output
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if((N<2)||(N>20)){
+            System.out.println("NA");
+        }
+        else{
+            for(int i=1;i<=10;i++){
+                System.out.println(N + " x " + i + " = " + N*i);
+            }
+        }
+        scanner.close();
+    }
+}
