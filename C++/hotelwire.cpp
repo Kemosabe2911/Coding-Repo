055099457074a583ddefb7cbec82fbf0966439e1
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 11:35:01 +0530

    Update hotelwire

--- C++/hotelwire.cpp
+++ C++/hotelwire.cpp
@@ -3,11 +3,26 @@
 using namespace std;
 
 int main(){
-    int m,n,k,i,count=0;
+    int m,n,k,i,j,small,pos=-1;
     int a[100];
     cin>>m>>n>>k;
     for(i=0;i<m;i++){
         cin>>a[i];
     }
+    for(i=0;i<k;i++){
+        small=999;
+        for(j=0;j<m;j++){
+            if(small>a[j]){
+                small=a[j];
+                pos=j;
+            }
+        }
+        a[pos]=n-a[pos];
+    }
+    int sum=0;
+    for(i=0;i<m;i++){
+        sum+=a[i];
+    }
+    cout<<sum;
     return 0;
 }
