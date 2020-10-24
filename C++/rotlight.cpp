56c4effafe00f2c20681729ae65a5d423fb6522f
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 13:54:57 +0530

    Update cpp

--- C++/rotlight.cpp
+++ C++/rotlight.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 using namespace std;
-
+/*
 void rightrot(int a[], int n){
     int temp1=a[n-1],i;
     for(i=0;i<n;i++){
         a[i+1]=a[i];
     }
     a[0]=temp1;
-
-}
+}*/
 
 int main(){
     int n,t,p,flg=0;
@@ -29,12 +28,19 @@ int main(){
         vis[j]=1;
     }
     for(i=0;i<t;i++){
+        cout<<init[i];
+    }
+    for(i=0;i<t;i++){
             arr[i]=init[i];
             //rightrot(arr[k],t);
     }
     int k=0;
     while(1){
-        rightrot(arr,t);
+        temp=arr[t-1];
+        for(i=0;i<t;i++){
+            arr[i+1]=arr[i];
+        }
+        arr[0]=temp;
         for(p=0;p<t;p++){
             if(init[p]==arr[p]){
                 flg=0;
