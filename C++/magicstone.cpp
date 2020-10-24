2547a3d9ca2b57ead82b84660647aee8e87c52dd
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 15:21:48 +0530

    Update cpp

--- C++/magicstone.cpp
+++ C++/magicstone.cpp
@@ -1,15 +1,32 @@
 #include<iostream>
 using namespace std;
 
+int countPairs(int arr[], int n) 
+{ 
+    int ans = 0; 
+  
+    // for each index i and j 
+    for (int i = 0; i < n; i++) 
+        for (int j = i+1; j < n; j++) 
+  
+            // finding the index with same 
+            // value but different index. 
+            if (arr[i] == arr[j]) 
+                ans++; 
+    return ans; 
+} 
+
 int main(){
-    int n,q,i,j,temp;
-    int val[100],a[100],k[100];
+    int n,q,i,j,temp,temp2,p;
+    int pos=-1,count=0;
+    int val[100],a[100],k[100],ind[100];
     cin>>n;
     for(i=0;i<n;i++){
         cin>>val[i];
+        a[i]=val[i];
     }
     for(i=0;i<n;i++){
-        a[i]=i;
+        ind[i]=i;
     }
     cin>>q;
     for(i=0;i<q;i++){
@@ -17,11 +34,26 @@ int main(){
     }
     temp=n;
     for(i=0;i<q;i++){
-        for ( j = 0; j < temp; j++)
-        {
-            /* code */
-            
+        for(p=0;p<temp;p++){
+            a[p]=val[p];
         }
-        
+        temp2=countPairs(a,temp);
+        if(temp2==0){
+            cout<<"-1 \n";
+            continue;
+        }
+        //cout<<temp2<<"\n";
+        temp=temp-temp2;
+        if(temp2>1){
+            temp=temp+1;
+        }
+        //cout<<temp<<"\n";
+        if(k[i]==temp){
+            cout<<i+1<<"\n";
+        }
+        else
+        {
+            cout<<"-1\n";
+        }
     }
 }
