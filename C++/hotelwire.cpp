0bca8a35de10c9c2342c7b79c02b6482f154bb76
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 11:23:21 +0530

    Add hotelwire

---
+++ C++/hotelwire.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int m,n,k,i,count=0;
+    int a[100];
+    cin>>m>>n>>k;
+    for(i=0;i<m;i++){
+        cin>>a[i];
+    }
+    return 0;
+}
