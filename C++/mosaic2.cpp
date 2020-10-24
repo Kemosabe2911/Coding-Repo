ae0b9773c9eada4df3f0315920bf47310be48762
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 18:56:30 +0530

    Update cpp

--- C++/mosaic2.cpp
+++ C++/mosaic2.cpp
@@ -1,16 +1,37 @@
 #include<iostream>
 using namespace std;
+class GFG 
+{ 
+      
+/* function to multiply two numbers x and y*/
+public : int multiply(int x, int y) 
+{ 
+    /* 0 multiplied with anything gives 0 */
+    if(y == 0) 
+    return 0; 
+  
+    /* Add x one by one */
+    if(y > 0 ) 
+    return (x + multiply(x, y-1)); 
+  
+    /* the case where y is negative */
+    if(y < 0 ) 
+    return -multiply(x, -y); 
+} 
+}; 
+
 int main(){
     int w,h,a,b,m,c;
     cin>>w>>h>>a>>b>>m>>c;
-    int tiles,flg=0;
+    int flg=0;
+    long tiles;
     int temp1,temp2;
     int inch;
     temp1=w/a;
     temp2=h/b;
-    cout<<temp1<<" "<<temp2;
+    cout<<temp1<<" "<<temp2<<"\n";
     tiles=temp1*temp2;
-    //cout<<tiles;
+    cout<<tiles;
     if(w%a > 0){
         tiles+=(h/b);
         flg=1;
