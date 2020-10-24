a5c4bfbba10a9bc96dd560b9ed44815e2c6a2174
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 13:51:03 +0530

    Update cpp

--- C++/rotlight.cpp
+++ C++/rotlight.cpp
@@ -2,55 +2,56 @@
 using namespace std;
 
 void rightrot(int a[], int n){
-    int temp,i,j;
-    for(j=0;j<n;j++){
-
-    }
+    int temp1=a[n-1],i;
     for(i=0;i<n;i++){
         a[i+1]=a[i];
     }
-    a[i]=temp;
+    a[0]=temp1;
+
 }
 
 int main(){
-    int n,t,p;
-    int temp;
-    int arr[100][100];
-    int vis[100];
+    int n,t,p,flg=0;
     int i,j;
+    int temp;
+    int arr[100];
+    int init[100];
+    int a[100];
+    int vis[100];
     cin>>n>>t;
     for(i=0;i<t;i++){
-        arr[0][i]=0;
+        init[i]=0;
         vis[i]=0;
     }
     for(i=0;i<n;i++){
         cin>>j;
-        arr[0][j]=1;
+        init[j]=1;
         vis[j]=1;
     }
-    int k=1;
-    while(1){
-        for(i=0;i<t;i++){
-            arr[k][i]=arr[k-1][i];
+    for(i=0;i<t;i++){
+            arr[i]=init[i];
             //rightrot(arr[k],t);
-        }
-        for(p=0;p<k;p++){
-            temp=arr[k][t-1];
-            for(i=0;i<n;i++){
-                arr[k][i+1]=arr[k][i];
+    }
+    int k=0;
+    while(1){
+        rightrot(arr,t);
+        for(p=0;p<t;p++){
+            if(init[p]==arr[p]){
+                flg=0;
             }
-            arr[k][0]=temp;
+            else
+            {
+                /* code */
+                flg=1;
+                k++;
+                break;
+            }
+            
         }
-        k++;
-        if(k==t){
+        if(flg==0){
             break;
         }
     }
-    for(i=0;i<t;i++){
-        for(j=0;j<t;j++){
-            cout<<arr[i][j];
-        }
-    cout<<"\n";
-    }
+    cout<<k;
     return 0;
 }
