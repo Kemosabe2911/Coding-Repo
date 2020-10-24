580b907849588d1564e370bee7a3a5a07c094619
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 18:47:59 +0530

    Update cpp

--- C++/mosaic2.cpp
+++ C++/mosaic2.cpp
@@ -3,12 +3,13 @@ using namespace std;
 int main(){
     int w,h,a,b,m,c;
     cin>>w>>h>>a>>b>>m>>c;
-    int tiles=0,flg=0;
+    int tiles,flg=0;
     int temp1,temp2;
     int inch;
     temp1=w/a;
     temp2=h/b;
-    tiles=(w/a)*(h/b);
+    cout<<temp1<<" "<<temp2;
+    tiles=temp1*temp2;
     //cout<<tiles;
     if(w%a > 0){
         tiles+=(h/b);
