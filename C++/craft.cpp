63133eb73ccb64948a5e89e6149ee91f2bca3662
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 11:09:24 +0530

    Update vcraft

--- C++/craft.cpp
+++ C++/craft.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 
-using namespace <std>
+using namespace std;
 
 int main(){
     int c,n,p,w;
     cin>>c>>n>>p>>w;
+    int i,temp;
+    int a[100];
+    i=0;
+    while(i<n){
+        if(w==0){
+            cout<<"0";
+            break;
+        }
+        else if(w-p<0){
+            a[i]=w;
+        }
+        else{
+            a[i]=p;
+            w=w-p;
+        }
+        i++;
+    }
+    for(i=0;i<n;i++){
+        cout<<a[i]<<"\n";
+    }
     return 0;
 }
