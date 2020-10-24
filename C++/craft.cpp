30ac37174d8e69760bc00a8cf4927da4a8f40d1f
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 11:16:44 +0530

    Update vcraft

--- C++/craft.cpp
+++ C++/craft.cpp
@@ -2,18 +2,26 @@
 
 using namespace std;
 
+int largest(int a[], int n){
+    int large=0;
+    for(int i=0; i<n; i++){
+        if(a[i]>large){
+            large=a[i];
+            pos=i;
+        }
+    }
+    return large;
+}
+
 int main(){
     int c,n,p,w;
     cin>>c>>n>>p>>w;
     int i,temp;
+    int count=0;
     int a[100];
     i=0;
     while(i<n){
-        if(w==0){
-            cout<<"0";
-            break;
-        }
-        else if(w-p<0){
+        if(w-p<0){
             a[i]=w;
         }
         else{
@@ -22,8 +30,13 @@ int main(){
         }
         i++;
     }
-    for(i=0;i<n;i++){
+    /*for(i=0;i<n;i++){
         cout<<a[i]<<"\n";
+    }*/
+    if(w<c){
+        cout<<"0";
     }
+    
+
     return 0;
 }
