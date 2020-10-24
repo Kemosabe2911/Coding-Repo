261353837001215ce1d2c8930a7237eb365467c6
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 18:07:48 +0530

    Update cpp

---
+++ C++/mosaic2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int w,h,a,b,m,c;
+    cin>>w>>h>>a>>b>>m>>c;
+    int tiles=0;
+    int temp1,temp2;
+    temp1=w/a;
+    temp2=h/b;
+    tiles=tiles+(temp1*temp2);
+    //cout<<tiles;
+    
+}
