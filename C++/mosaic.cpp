2d19a124cefc47aed12bb21134a870cb93b4e543
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-24 10:07:16 +0530

    Update mosaic

--- C++/mosaic.cpp
+++ C++/mosaic.cpp
@@ -6,6 +6,7 @@ int main() {
     int n,cb,cp,i;
     int b[100],p[100];
     int sumb=0,sump=0;
+    int bc,pc;
     cin>>n>>cb>>cp;
     for (i = 0; i < n; i++)
     {
@@ -14,7 +15,27 @@ int main() {
         sumb+=b[i];
         sump+=p[i];
     }
-    cout<<sumb<<sump;
-    
+    //cout<<sumb<<sump;
+    if(sumb%10==0){
+        bc=sumb/10;
+    }
+    else
+    {
+        /* code */
+        bc=sumb/10;
+        bc++;
+    }
+    if(sump%10==0){
+        pc=sump/10;
+    }
+    else
+    {
+        /* code */
+        pc=sump/10;
+        pc++;
+    }
+    //cout<<bc<<pc;
+    int output= (bc*cb)+(pc*cp);
+    cout<<output;
     return 0;
 }
