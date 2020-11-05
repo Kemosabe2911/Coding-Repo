e9043f9e38f64f254a0acc900ae63b9d96b250bf
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-05 23:16:58 +0530

    Update exp11

--- C/SS Lab/exp11/exp11.c
+++ C/SS Lab/exp11/exp11.c
@@ -31,6 +31,7 @@ return -1;
 }
 int check_macro(char x[20])
 {
+
 int i;
 for(i=0;i<m;i++)
 if(strcmp(nt[i].name,x)==0)
@@ -40,40 +41,42 @@ return -1;
 void store()
 {
 FILE *f,*fp;
-int k,i,j;
+int  k,i,j;
 char a[20],b[20],c[40],temp[20],lab[20];
 n=0;p=0;m=0;g=0;
 f=fopen("input.txt","r");
 fp=fopen("exp.txt","w");
 while(!feof(f))
 {
+
 fscanf(f,"%s\t%s\t%s\n",a,b,c);
 if(strcmp(b,"MACRO")==0)
 {
-strcpy(nt[m].name,a);
-nt[m].start=0;
-k=1;
-j=0;
-i=0;
-while(c[i]!='\0')
-{
-if(c[i]!=',')
-{
-temp[j]=c[i];
-j++;
-}
-else
-{
-temp[j]='\0';
-j=0;
-strcpy(pt[p].param,temp);
-pt[p].val[0]='?';
-pt[p].val[1]=p+49;
-pt[p].val[2]='\0';
-p++;
-}
-i++;
-}
+        strcpy(nt[m].name,a);
+        nt[m].start=0;
+        k=1;
+        j=0;
+        i=0;
+        while(c[i]!='\0')
+        {
+        if(c[i]!=',')
+                {
+                        temp[j]=c[i];
+                        j++;
+                }
+                else
+                {
+                temp[j]='\0';
+                j=0;
+                strcpy(pt[p].param,temp);
+                pt[p].val[0]='?';
+                pt[p].val[1]=p+49;
+                pt[p].val[2]='\0';
+                p++;
+
+                }
+                i++;
+        }
 temp[j]='\0';
 strcpy(pt[p].param,temp);
 pt[p].val[0]='?';
@@ -94,48 +97,51 @@ else
 strcpy(dt[n].operand,pt[i].val);
 n++;
 if(strcmp(b,"MEND")==0)
-{
-k=0;
-nt[m].end=n-1;
-m++;
-}
+        {
+        k=0;
+        nt[m].end=n-1;
+        m++;
+        }
 }
 else if((check_macro(b))!=-1)
 {
 i=0;
-g=0;
-j=0;
-while(c[i]!='\0')
-{
-if(c[i]!=',')
-{
-temp[j]=c[i];
-j++;
-}
-else
-{
-temp[j]='\0';
-j=0;
-strcpy(at[g].arg,temp);
-g++;
-}
-i++;
-}
-temp[j]='\0';
-strcpy(at[g].arg,temp);
-g++;
-strcpy(a,"NULL");
-for(i=1;i<n-1;i++)
-{
-if(dt[i].operand[0]=='?')
-{
-j=dt[i].operand[1]-49;
-strcpy(temp,at[j].arg);
-}
-else
-strcpy(temp,dt[i].operand);
-fprintf(fp,"%s\t%s\t%s\n",a,dt[i].opcode,temp);
-}
+        g=0;
+        j=0;
+        while(c[i]!='\0')
+        {
+                if(c[i]!=',')
+                {
+                temp[j]=c[i];
+                j++;
+                }
+                else
+                {
+                temp[j]='\0';
+                j=0;
+                strcpy(at[g].arg,temp);
+
+                g++;
+
+                }
+                i++;
+
+        }
+        temp[j]='\0';
+        strcpy(at[g].arg,temp);
+        g++;
+        strcpy(a,"NULL");
+        for(i=1;i<n-1;i++)
+        {
+        if(dt[i].operand[0]=='?')
+        {
+        j=dt[i].operand[1]-49;
+        strcpy(temp,at[j].arg);
+        }
+        else
+        strcpy(temp,dt[i].operand);
+        fprintf(fp,"%s\t%s\t%s\n",a,dt[i].opcode,temp);
+        }
 }
 else
  fprintf(fp,"%s\t%s\t%s\n",a,b,c);
