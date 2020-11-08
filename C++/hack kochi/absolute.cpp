037a00360892b3d31c5c5fd42e2ffc8821118573
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-08 17:28:47 +0530

    Update absolute.cpp

--- C++/hack kochi/absolute.cpp
+++ C++/hack kochi/absolute.cpp
@@ -7,18 +7,43 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n;
     int a[100];
     int p[10],q[10];
     cin>>n;
-    int i;
+    int c=0,b=0;
+    int i,j, diff ,small=99999;
     for (i=0 ; i < n; i++)
     {
         /* code */
         cin>>a[i];
     }
-    
-    
+    for( i=0;i< n;i++){
+        for( j=i; j<n;j++){
+            if(i!=j){
+                diff = a[i] - a[j];
+                if(diff < 0){
+                    diff= diff*(-1);
+                }
+                if(small > diff){
+                    c=0;
+                    b=0;
+                    p[c] = a[i];
+                    q[b] = a[j];
+                    small=diff;
+                }
+                if(small == diff){
+                    c++;
+                    b++;
+                    p[c]=a[i];
+                    q[b]=a[j];
+                }    
+            }
+        }
+    }
+    for(i=0;i<=c;i++){
+        cout<< p[i] << " "<< q[i] << " ";
+    }
     return 0;
 }
