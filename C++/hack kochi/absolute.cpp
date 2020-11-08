a25be3d7151e2997494b6d20e9d38a93103dfbb6
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-08 17:11:40 +0530

    Add absoute.cpp

---
+++ C++/hack kochi/absolute.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    int a[100];
+    int p[10],q[10];
+    cin>>n;
+    int i;
+    for (i=0 ; i < n; i++)
+    {
+        /* code */
+        cin>>a[i];
+    }
+    
+    
+    return 0;
+}
