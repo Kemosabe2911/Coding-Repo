62f968a16af3cd4b7e6cad6c0df5a06314d38ba8
Kemosabe2911 <stevinprince29@gmail.com>
2021-01-19 10:49:18 +0530

    Add pass1

---
+++ C/SS Lab/rev/pass1/pass1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main(){
+    int locc,flag=0;
+    char first[20],second[20],third[20],opfirst[20],opsecond[20];
+    FILE *input,*symtab,*optab,*inter;
+
+}
